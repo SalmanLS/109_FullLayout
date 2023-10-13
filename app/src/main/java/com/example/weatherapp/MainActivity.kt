4ad73cf46289db6bf4d6b053507f979e6177dcd3
SalmanLS <salmanluthfi456@gmail.com>
2023-10-13 20:42:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.example.weatherapp
+
+import android.graphics.Paint.Align
+import android.media.Image
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+        Text(
+            text = "WEATHER APP",
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+
+            )
+
+        Spacer(modifier = Modifier.height(13.dp))
+
+        Box1()
+
+        Spacer(modifier = Modifier.height(13.dp))
+
+        Location()
+
+    }
+}
+
+@Composable
+fun Box1() {
+    val img = painterResource(id = R.drawable.img)
+
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .fillMaxWidth()
+            .background(color = Color.Cyan)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(
+                painter = img,
+                contentDescription = null,
+                modifier = Modifier.size(150.dp)
+            )
+
+            Text(
+                text = "Rain",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(13.dp)
+            )
+        }
+    }
+
+
+}
+
+@Composable
+fun Location() {
+    Text(text = "19°C", fontSize = 64.sp, fontWeight = FontWeight.Bold)
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        androidx.compose.foundation.Image(
+            painter = painterResource(id = R.drawable.baseline_location_on_24),
+            contentDescription = "",
+            modifier = Modifier.size(35.dp)
+        )
+        Spacer(modifier = Modifier.padding(3.dp))
+        Text(text = "Yogyakarta", fontSize = 40.sp, fontWeight = FontWeight.Bold)
+    }
+    
+    Spacer(modifier = Modifier.padding(13.dp))
+    
+    Text(text = "Kasihan, Kabupaten Bantul, Daerah Istimewa Yogyakarta",
+        textAlign = TextAlign.Center,
+        fontSize = 20.sp,
+        color = Color.Gray
+        )
+
+}
+
